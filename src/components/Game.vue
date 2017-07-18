c8ce4e8199c39b1e56923cc5dcc1a78c299a259d
Quapyrus <quapyrus@gmail.com>
2017-07-18 14:06:45 +0800

    init project

---
+++ src/components/Game.vue
@@ -0,0 +1,24 @@
+<template>
+  <table id="qs-elevator">
+    <tr>
+      <td>sb</td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'qs-elevator'
+}
+</script>
+
+<style>
+#qs-elevator {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
